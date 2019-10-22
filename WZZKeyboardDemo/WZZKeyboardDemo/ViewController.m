b255a5072df1326d072fde2730c6546bb48583ec
wzz <714318973@qq.com>
2019-10-22 14:27:06 +0800

    ff

--- WZZKeyboardDemo/WZZKeyboardDemo/ViewController.m
+++ WZZKeyboardDemo/WZZKeyboardDemo/ViewController.m
@@ -19,5 +19,13 @@
     // Do any additional setup after loading the view.
 }
 
+- (IBAction)useWZZKeyboardM:(id)sender {
+    UIView * ff = [[UIView alloc] initWithFrame:CGRectMake(20.0f, [UIScreen mainScreen].bounds.size.height/2.0f-20.0f, [UIScreen mainScreen].bounds.size.width-40.0f, 40.0f)];
+    [[UIApplication sharedApplication].keyWindow addSubview:ff];
+    UITextField * f2 = [[UITextField alloc] initWithFrame:ff.bounds];
+    [ff addSubview:f2];
+    ff.backgroundColor = [UIColor groupTableViewBackgroundColor];
+    f2.placeholder = @"此处使用WZZKeyboardManager";
+}
 
 @end
